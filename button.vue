28f3f1302e7dcf572b87cadafefc8ae9a996f42b
aka <akadev8@outlook.com>
2024-10-03 16:01:47 -0700

    Create button.vue

---
+++ button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="td-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'TButton',
+};
+</script>
+
+<style scoped>
+.td-button {
+  /* Add your styles here */
+}
+</style>
