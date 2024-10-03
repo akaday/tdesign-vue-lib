5fc5485f54d3a105a1ba83e3630aa6a14f60bd0d
aka <akadev8@outlook.com>
2024-10-03 16:39:33 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
-import TButton from './components/TButton.vue';
+import TButton from './components/Button.vue';
 
 const app = createApp(App);
 app.component('TButton', TButton);
