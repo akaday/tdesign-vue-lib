d9b189f22452535243e65a90732572e0e3d5530f
aka <akadev8@outlook.com>
2024-10-03 17:00:47 -0700

    Update and rename button.vue to Button.vue

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="t-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'TButton',
+};
+</script>
+
+<style scoped>
+.t-button {
+  padding: 10px 20px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
